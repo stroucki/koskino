d3b9a1b7e444c17df55566d4825717a44c2c6966
Michael Stroucken <mxs@cmu.edu>
2014-05-19 15:50:58 -0400

    MainOptions: make unused parameters not required

--- src/main/java/com/github/anastasop/koskino/MainOptions.java
+++ src/main/java/com/github/anastasop/koskino/MainOptions.java
@@ -8,9 +8,9 @@ public class MainOptions {
 	@Option(name = "-p", aliases = {"--port"}, usage = "server port. Default 40000", required = false)
 	public Integer port = 40000;
 
-	@Option(name = "-d", aliases = {"--arenas"}, usage = "directory to store arenas", required = true)
+	@Option(name = "-d", aliases = {"--arenas"}, usage = "directory to store arenas", required = false)
 	public File arenaDir;
 
-	@Option(name = "-u", aliases = {"--use"}, usage = "arena name to use", required = true)
+	@Option(name = "-u", aliases = {"--use"}, usage = "arena name to use", required = false)
 	public String arenaName;
 }
